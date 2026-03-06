2284dbaa3f280e78caea4bfe7b7fc49d4ef80423
jellomoon <damonita@gmail.com>
2026-03-06 14:00:43 +0100

    Added return to main page option

--- javascript/index.js
+++ javascript/index.js
@@ -48,7 +48,8 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
         <div class="time">${cityTime.format("HH:mm:ss")}</div>
-        </div>`;
+        </div>
+        <a href="/">Back to Overview</a>`;
 }
 
 updateTime();
